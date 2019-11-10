63269dcb2f9b03832d2eb5f47d15582052d42fc9
Jeremy Hon <jehon11@gmail.com>
2019-11-10 14:10:43 +0900

    can scrape

--- services/scraper.js
+++ services/scraper.js
@@ -1,9 +1,25 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
-const url = 'https://www.toyota.com/rav4/';
+const urls = {
+  RAV4: 'https://www.toyota.com/camry/',
+  Camry: 'https://www.toyota.com/camry/',
+  Corolla: 'https://www.toyota.com/corolla/',
+  Tacoma: 'https://www.toyota.com/tacoma/',
+  Highlander: 'https://www.toyota.com/highlander/',
+  '4Runner': 'https://www.toyota.com/4runner/',
+  Tundra: 'https://www.toyota.com/tundra/',
+  Prius: 'https://www.toyota.com/prius/',
+  Yaris: 'https://www.toyota.com/yaris/'
+};
 
-axios.get(url)
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => console.log(error));
+Object.entries(urls).forEach(([name, url]) => {
+  axios.get(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const msrp = $('.mlp-welcome-msrp').find('strong').text().trim();
+      console.log(`${name}: ${msrp}`);
+    })
+    .catch((error) => console.log(error));
+});
