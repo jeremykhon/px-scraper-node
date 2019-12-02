c7f995197e18a7dbac608b5c7d1224851ce25aea
Jeremy Hon <jehon11@gmail.com>
2019-12-02 22:53:14 +0900

    db

--- db.js
+++ db.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const db = new Client({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
 });
 
 async function dbStart() {
