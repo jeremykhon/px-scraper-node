08a0928c5ee796c0627272ba4907cbf631d35fc9
Jeremy Hon <jehon11@gmail.com>
2019-12-02 22:53:31 +0900

    migration create table

---
+++ migrations/20191202123833-CreateCarPriceLogs.js
@@ -0,0 +1,33 @@
+let dbm;
+let type;
+let seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('CarPriceLogs', {
+    id: {
+      type: 'int',
+      primaryKey: true,
+      unsigned: true,
+      autoIncrement: true,
+    },
+    carName: { type: 'string', notNull: true },
+    carBrand: { type: 'string', notNull: true },
+    carPrice: 'int',
+    url: 'string',
+    createdAt: { type: 'timestamp', notNull: true },
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('CarPriceLogs');
+};
