fd8a3c63e7b4eb32a044eb2e69511d21e7f547bc
Jeremy Hon <jehon11@gmail.com>
2019-12-06 23:01:56 +0900

    add more cars and patterns

--- services/scraper.js
+++ services/scraper.js
@@ -24,7 +24,7 @@ const chromeOptions = {
 };
 
 function scraper() {
-  const fetchPrice = async (carName, url, carBrand) => {
+  const fetchPrice = async (carName, url, carBrand, carPattern) => {
     const logPrice = async (carPrice) => {
       try {
         const timestamp = Date.now() / 1000;
@@ -38,19 +38,72 @@ function scraper() {
     };
 
     const findCarPrice = ($) => {
-      switch (carBrand) {
-        case 'Toyota':
+      switch (carPattern) {
+        case 'toyota':
           return $('.mlp-welcome-msrp')
             .find('strong')
             .text()
-            .trim()
-            .replace(/[,$]/g, '');
-        case 'Nissan':
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
+        case 'nissan':
           return $('.primary-price')
             .find('strong')
             .first()
             .text()
-            .replace(/[,$]/g, '');
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
+        case 'honda':
+          return $('.left-content.purchase-info')
+            .find('h2')
+            .text()
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
+        case 'subaru':
+          return $('.spec-element-one')
+            .find('span.font-sizes-big')
+            .text()
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
+        case 'mazda':
+          return $('.mde-home-trims__alternate--msrp')
+            .find('p')
+            .first()
+            .children()
+            .remove()
+            .end()
+            .text()
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
+        case 'mazda1':
+          return $('.mdp-flexiblecontent-trims-carousel__slide-blurb.mdp-foundation-copy')
+            .find('p')
+            .first()
+            .children()
+            .remove()
+            .end()
+            .text()
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
+        case 'mazda2':
+          return $('.mde-home-trims__trim--msrp')
+            .find('p')
+            .first()
+            .children()
+            .remove()
+            .end()
+            .text()
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
+        case 'lexus':
+          return $('div.msrp')
+            .find('div.price')
+            .first()
+            .attr('data-msrp');
+        case 'mitsubishi':
+          return $('div.pricing')
+            .find('p.item-value')
+            .children()
+            .remove()
+            .end()
+            .text()
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
+        case 'acura':
+          return $('span.price.acr-caption-3')
+            .text()
+            .replace(/[A-z]|[,$ *]|[\u25A0\u00A0\s]/g, '');
         default:
           return null;
       }
@@ -76,9 +129,8 @@ function scraper() {
   const execute = () => {
     dbStart();
     Object.entries(carData).forEach(([carName, carProperties]) => {
-      queue.add(() => fetchPrice(carName, carProperties.url, carProperties.brand));
+      queue.add(() => fetchPrice(carName, carProperties.url, carProperties.brand, carProperties.pattern));
     });
-    console.log('done scraping');
   };
 
   execute();
