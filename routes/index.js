31a43551eba6196671b0cf9f5a1f65f4fab6958b
Jeremy Hon <jehon11@gmail.com>
2019-12-06 23:02:13 +0900

    error handling in route

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ const { db } = require('../db');
 const router = express.Router();
 const sql = fs.readFileSync(path.join(__dirname, '/../sql/query.sql')).toString();
 
-router.get('/source', (req, res) => {
-  res.render('source', { title: 'source' });
-});
-
-router.get('/', (req, res) => {
-  const getData = async () => {
-    const data = await db.query(sql);
-    res.render('index', { title: 'Car Prices', rows: data[1].rows, fields: data[1].fields });
-  };
-  getData();
+router.get('/', (req, res, next) => {
+  try {
+    const getData = async () => {
+      const data = await db.query(sql);
+      res.render('index', { title: 'Car Prices', rows: data[1].rows, fields: data[1].fields });
+    };
+    getData();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
