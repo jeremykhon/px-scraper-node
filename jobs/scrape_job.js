41cf99e6bfd363985a1859b14426ae8479e92fd2
Jeremy Hon <jehon11@gmail.com>
2019-12-14 00:32:19 +0900

    scrape job

--- jobs/scrape_job.js
+++ jobs/scrape_job.js
@@ -1,3 +1,23 @@
-const { scraper } = require('../services/scraper');
+const Queue = require('bull');
+const scrape = require('../services/scraper/scrape');
 
-scraper();
+const scraperQueue = new Queue('scraping', process.env.REDIS_URL);
+
+scraperQueue.on('log error', (e) => {
+  console.error(e, e.stack);
+});
+
+scraperQueue.on('completed', (job) => {
+  console.log(`Job with id ${job.id} completed, car: ${job.data.carName}`);
+});
+
+scraperQueue.process('scrape job', async (job) => {
+  try {
+    await scrape(job.data);
+  } catch (e) {
+    scraperQueue.emit('log error', e);
+    throw e;
+  }
+});
+
+module.exports = scraperQueue;
